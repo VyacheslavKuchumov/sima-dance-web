50a9accdd190d76d2b89cb76b8ed040e694bdf39
VyacheslavKuchumov <Kuchumov76@mail.ru>
2025-03-16 20:54:27 +0500

    stupid enums

--- backend/app/models/seats_in_events.py
+++ backend/app/models/seats_in_events.py
@@ -1,15 +1,10 @@
-from sqlalchemy import Column, BigInteger, ForeignKey, Integer, Enum, UUID
+from sqlalchemy import Column, BigInteger, ForeignKey, Integer, Enum, UUID, String
 from sqlalchemy.orm import relationship
 from datetime import datetime
 import enum
 from app.database import Base
 
 # Define enum for seat status
-class SeatStatus(enum.Enum):
-    available = "available"
-    held = "held"
-    booked = "booked"
-    unavailable = "unavailable"
 
 class SeatInEvent(Base):
     __tablename__ = 'seats_in_events'
@@ -17,7 +12,7 @@ class SeatInEvent(Base):
     seat_in_event_id = Column(BigInteger, primary_key=True)
     seat_id = Column(BigInteger, ForeignKey('seats.seat_id'), nullable=False)
     event_uid = Column(UUID, ForeignKey('events.event_uid'), nullable=False)
-    status = Column(Enum(SeatStatus), default=SeatStatus.available, nullable=False)
+    status = Column(String, default="available", nullable=False)
     price = Column(Integer, nullable=True)
     
     seat = relationship("Seat", uselist=False, back_populates="seat_in_event")
