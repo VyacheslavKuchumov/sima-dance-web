643aef8a6ab726911e6aed1f7e787e057ac6d017
VyacheslavKuchumov <Kuchumov76@mail.ru>
2025-04-12 22:09:05 +0500

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,8 +6,8 @@ from app.config import settings
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL,
-                       pool_size=15,         # The number of connections to keep open inside the pool.
-    max_overflow=10,     # The maximum number of connections that can be created beyond the pool_size.
+                       pool_size=100,         # The number of connections to keep open inside the pool.
+    max_overflow=50,     # The maximum number of connections that can be created beyond the pool_size.
     pool_timeout=30,     # The maximum number of seconds to wait for a connection.
     pool_recycle=1800,  # The number of seconds after which a connection is automatically recycled.
     pool_pre_ping=True)  # Check if the connection is still valid before using it.
