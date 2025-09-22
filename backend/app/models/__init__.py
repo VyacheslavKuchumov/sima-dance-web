262e4456bde4f963ea58849bb863154cbf44eade
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2025-09-22 10:55:11 +0500

    init frontend

--- backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# from .auth import *
-# from .user import *
-
-# from .venues import *
-# from .seats import *
-# from .events import *
-# from .seats_in_events import *
-# from .bookings import *
--- backend/app/redis_client.py
+++
@@ -1,4 +0,0 @@
-import redis
-
-def get_redis_client():
-    return redis.Redis(host='localhost', port=6379, db=0)
